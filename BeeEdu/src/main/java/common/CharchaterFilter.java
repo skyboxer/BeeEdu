d52d1248ef5fa8d89d705ea3b5d08ac9fa152242
wc15250138779 <1960578966@qq.com>
2019-11-28 14:10:47 +0800

    过滤器修改 王成

--- BeeEdu/src/main/java/common/CharchaterFilter.java
+++ BeeEdu/src/main/java/common/CharchaterFilter.java
@@ -27,7 +27,6 @@ public class CharchaterFilter implements Filter {
         HttpSession session = request.getSession();
         //拿到用户登录信息
         User user = (User) session.getAttribute("user");
-        System.out.println("user = " + user);
 
         // 获得用户请求的URI
         String path = request.getRequestURI();
@@ -39,14 +38,6 @@ public class CharchaterFilter implements Filter {
         if(method.equalsIgnoreCase("post")){
             request.setCharacterEncoding("utf-8");
         }
-
-        //静态资源无需过滤
-        if(path.indexOf(".css" ) > -1 || path.indexOf(".js") > -1 || path.indexOf(".ttf") > -1
-                || path.indexOf(".woff") > -1 || path.indexOf(".woff2") > -1 ){
-            filterChain.doFilter(request, response);
-            return;
-        }
-
         // 登陆页面无需过滤
         if(path.indexOf("/login.html") > -1 || path.indexOf("/userController") > -1)  {
             System.out.println("path = " + path);
@@ -54,16 +45,27 @@ public class CharchaterFilter implements Filter {
             filterChain.doFilter(request, response);
             return;
         }
-        // 判断如果没有取到员工信息,就跳转到登陆页面
-        if (user == null) {
-            // 跳转到登陆页面
-            System.out.println("path = " + path);
-            response.sendRedirect("/login.html");
-        } else {
-            response.setContentType("text/html;charset=utf-8");
-            // 已经登陆,继续此次请求
-            filterChain.doFilter(request, response);
+        //过滤带.html后缀的
+        if (path.indexOf(".html") > -1){
+            System.out.println("user = " + user);
+
+            // 判断如果没有取到员工信息,就跳转到登陆页面
+            if (user == null) {
+                // 跳转到登陆页面
+                System.out.println("path = " + path);
+                response.sendRedirect("/login.html");
+                return;
+            } else {
+                response.setContentType("text/html;charset=utf-8");
+                // 已经登陆,继续此次请求
+                filterChain.doFilter(request, response);
+                return;
+            }
         }
+
+        filterChain.doFilter(request, response);
+
+
 
 
 
