5293aa6bf8ac43b35be9c6a9b744f75fb3f22755
wc15250138779 <1960578966@qq.com>
2019-12-04 16:32:30 +0800

    ssm框架搭建完成，登录功能实现，包结构更改 王成

---
+++ BeeEdu/src/main/java/com/enablue/controller/AcquiringTextVoice.java
@@ -0,0 +1,48 @@
+package com.enablue.controller;
+
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import com.enablue.util.WebTTSWS;
+
+
+/**
+ * 语音合成接口
+ */
+@WebServlet("/acquiringTextVoice")
+public class AcquiringTextVoice extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("utf-8");
+        resp.setContentType("text/html;charset=utf-8");
+        PrintWriter out = resp.getWriter();
+        String text = req.getParameter("Text");
+        if(text == null || text.length()==0) {
+            return;
+        }
+        try {
+            WebTTSWS webTTSWS = new WebTTSWS();
+            String projectPath=  getServletContext().getRealPath("/upload/");
+            System.out.print("项目目录："+projectPath);
+            String EntTo = "intp65";
+            String fileWavName = webTTSWS.getVoice(text,projectPath,EntTo);
+            out.println(fileWavName);
+            out.flush();
+            out.close();
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
---
+++ BeeEdu/src/main/java/com/enablue/mapper/AccountMapper.java
@@ -0,0 +1,15 @@
+package com.enablue.mapper;
+
+import org.apache.ibatis.annotations.Param;
+
+import com.enablue.pojo.Account;
+
+
+
+
+public interface AccountMapper {
+
+
+
+   Account queryAccount(@Param("name") String name, @Param("password") String password);
+}
