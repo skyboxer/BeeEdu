af7ba00997940408985da09dbfaf80c20dddc3d9
wc15250138779 <1960578966@qq.com>
2020-03-03 10:28:18 +0800

    智能题库

---
+++ SmartQuestionBank/src/main/java/com/enablue/service/AccountService.java
@@ -0,0 +1,23 @@
+package com.enablue.service;
+
+import com.enablue.pojo.Account;
+
+import java.util.HashMap;
+
+/**
+ * 用户服务接口
+ * 王成
+ * 2019.12.3 11:08
+ */
+
+public interface AccountService {
+     HashMap<String, Object> login(Account account);
+
+    HashMap<String, Object> queryAllAccount(Long page, Long limit);
+
+    HashMap<String, Object> addAccount(Account account);
+
+    HashMap<String, Object> deleteAccount(Long id);
+
+    HashMap<String, Object> updataAccount(Account account);
+}
