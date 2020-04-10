75b5920b3d12284ceae636f03851d3f8cf6f2bce
wc15250138779 <1960578966@qq.com>
2020-04-10 14:31:58 +0800

    增加js功能为获取URL的参数值 编辑类型功能保留原有参数 王成

---
+++ SmartQuestionBank/src/main/webapp/js/getValueFromURL.js
@@ -0,0 +1,16 @@
+//根据变量名获取URL中的值 未处理中文乱码
+function getQueryString(name) {
+    var reg = new RegExp('(^|&)' + name + '=([^&]*)(&|$)', 'i');
+    var r = window.location.search.substr(1).match(reg);
+    if (r != null) {
+        return unescape(r[2]);
+    }    function getQueryString(name) {
+        var reg = new RegExp('(^|&)' + name + '=([^&]*)(&|$)', 'i');
+        var r = window.location.search.substr(1).match(reg);
+        if (r != null) {
+            return decodeURI(r[2]);
+        }
+        return null;
+    }
+    return null;
+}
