17b56127f75e64c5a95ba5cfcf4cf9072d889786
cnxjk <cn_xjk@163.com>
2020-04-21 15:42:51 +0800

    追加上次提交

--- SmartQuestionBank/src/main/java/com/enablue/controller/CreateQuestionsController.java
+++ SmartQuestionBank/src/main/java/com/enablue/controller/CreateQuestionsController.java
@@ -44,7 +44,7 @@ public class CreateQuestionsController {
     @Autowired
     private BaseController baseController;
 
-    @RequestMapping("createQuestions")
+    @RequestMapping("model1")
     public JSONObject createQuestions(String title, String name, HttpServletRequest request){
         JSONObject resultObject = new JSONObject();
         User user = baseController.getSessionUser();
@@ -93,42 +93,11 @@ public class CreateQuestionsController {
             InputStream is = new FileInputStream(servletContext.getRealPath(templatePath));
             doc= new HWPFDocument(is);
             Range range = doc.getRange();
-            /*TableIterator tableIterator = new TableIterator(range);
-            while (tableIterator.hasNext()){
-                Table table = tableIterator.next();
-                for(int i = 0;i<table.numRows();i++){
-                    TableRow tableRow = table.getRow(i);
-                    for(int j =0;j<tableRow.numCells();j++){
-                        TableCell tableCell=tableRow.getCell(j);
-                        for(int k=0;k<tableCell.numParagraphs();k++){
-                            Paragraph paragraph = tableCell.getParagraph(k);
-                            for(JSONObject question : questionList){
-                                System.out.println(paragraph.getParagraph(k));
-                                paragraph.replaceText(question.getString("name"),question.getString("value"));
-                            }
-                        }
-                    }
-                }
-            }*/
             for(JSONObject question : questionList){
                 System.out.println(question.getString("name")+"  <><> "+question.getString("value"));
                 range.replaceText(question.getString("name"),question.getString("value"));
             }
-            /*String newPath =servletContext.getRealPath("/download/"+newFileName+".doc");
-            String downloadPath = servletContext.getRealPath("/download");
-            File downloadPathFile = new File(downloadPath);
-            File newDoc = new File(newPath);
-            if(!downloadPathFile.exists()){
-                downloadPathFile.mkdir();
-                System.out.println("创建文件价");
-            }
-            if(!newDoc.exists()){
-                newDoc.createNewFile();
-                System.out.println("创建文件");
-            }
-            outputStream = new FileOutputStream(newPath);*/
             String newDocPath = "/home/data/ROOT1/download/"+newFileName+".doc";
-            //String newDocPath = "/home/cnxjk/图片/"+newFileName+".doc";
             File newDocFile = new File(newDocPath);
             if(!newDocFile.isFile() && !newDocFile.exists()){
                 newDocFile.createNewFile();
@@ -137,7 +106,6 @@ public class CreateQuestionsController {
             outputStream = new FileOutputStream(newDocPath);
             doc.write(outputStream);
             String htmlPath = WordToHtml.Word2003ToHtml("/home/data/ROOT1/download/",newFileName,".doc","/home/data/ROOT1/download/");
-            //String htmlPath = WordToHtml.Word2003ToHtml("/home/data/ROOT1/download/",newFileName,".doc","/home/data/ROOT1/download/");
             System.out.println(htmlPath);
         } catch (IOException e) {
             e.printStackTrace();
